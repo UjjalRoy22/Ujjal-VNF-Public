adf3d79ea341edd5d2e7220578470d1ce6192de8
UjjalRoy22 <b18csel293@cit.ac.in>
2024-02-23 22:38:30 +0530

    rate

--- src/utils/users.tsx
+++ src/utils/users.tsx
@@ -6,7 +6,7 @@ export const userData =[
         role: "0",
         roleName: "Badger",
         password: "123",
-        ethadd: "0x536481d42C1ea9DBfeCfAFDc361B797f4dE56bdB",
+        ethadd: "0x120C5FbB0FED66A7CcA55a1f3f552Eb46649373F",
     },
     {
         id: "1",
@@ -15,7 +15,7 @@ export const userData =[
         role: "1",
         roleName: "Customer",
         password: "123",
-        ethadd: "0x6b4473109AF010A3FD995Ad29FB591A3674F2114"
+        ethadd: "0xF2beCaCAeCcd0171274515fc921bCF7810499EB2"
 
     },
    
